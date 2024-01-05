11e5b2d6984903606328477cac473ab9787693c2
Ramesh Neelala <neelalaramesh18@gmail.com>
2024-01-05 13:08:11 +0530

    Another Commit

--- src/components/Contacts/index.js
+++ src/components/Contacts/index.js
@@ -6,6 +6,7 @@ const Contacts = () => (
     <Header />
     <div className="contacts-container">
       <h1 className="heading">This is Contacts Page</h1>
+      <p>Yet to Build !!!!!!!</p>
     </div>
   </div>
 )
